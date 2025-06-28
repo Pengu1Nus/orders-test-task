c75bf72a79840f768022858399a7fa946c88378b
Pengu1Nus <mustafin.zul@gmail.com>
2025-06-28 16:59:50 +0300

    feat: Add users endpoint. Support CRUD operations. Add token auth

--- users/models.py
+++ users/models.py
@@ -6,7 +6,15 @@ from django.db import models
 class User(AbstractUser):
     """Модель пользователя."""
 
+    first_name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    username = models.CharField(
+        'Имя пользователя', max_length=150, unique=True
+    )
+    password = models.CharField(('Пароль'), max_length=128)
+
     age = models.PositiveSmallIntegerField(
+        'Возраст',
         null=True,
         blank=True,
         validators=[MaxValueValidator(120)],
--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import authentication, generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from .serializers import AuthTokenSerializer, UserSerializer
+
+User = get_user_model()
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Вью для создания пользователя."""
+
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Вью для обновления данных пользователя."""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Получить и вернуть аутентифицированного пользователя."""
+        return self.request.user
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Вью для обработки запросов токена."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
