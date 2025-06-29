c9fb3f59e314dabcc2abc130a6366b6535162fa7
Pengu1Nus <mustafin.zul@gmail.com>
2025-06-29 11:03:37 +0300

    fix: Move files into app folder

--- orders/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import rest_framework as filters
-
-from .models import Order
-
-
-class OrderFilter(filters.FilterSet):
-    class Meta:
-        model = Order
-        fields = ('name',)
